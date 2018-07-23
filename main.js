abc23c132c664c31c3151ea9783e9ec7ca3cd02a
jfrazier <johnfrazier1222@gmail.com>
2018-07-23 16:48:48 -0400

    new update

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ function getComicDc() {
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         var myComic = JSON.parse(this.responseText);
-        var pick = document.getElemetById;
-        pick('book1').innerHTML = myComic.company.dc.batman.name;
-
+        console.log(myComic.company.dc.batman.name);
+        document.getElementById('book1').innerHTML = myComic.company.dc.batman.name;
+        document.getElementById('release1').innerHTML = myComic.company.dc.batman.release;
+        document.getElementById('image1').src = myComic.company.dc.batman.image;
+        document.getElementById('para1').innerHTML = myComic.company.dc.batman.content;
       }
     };
     xhttp.open('GET', 'https://raw.githubusercontent.com/jafrazier/JSON/master/new.json', true);
     xhttp.send();
 }
-getElemetById
