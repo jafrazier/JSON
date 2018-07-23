94b9cdb1dbb78e3f7c747b4ae90bb458ad6fbb2a
jfrazier <johnfrazier1222@gmail.com>
2018-07-23 15:52:51 -0400

    update to json

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function getComic() {
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         var myComic = JSON.parse(this.responseText);
-        console.log(myComic.company);
+        console.log(myComic.company.dc.batman);
       }
     };
     xhttp.open('GET', 'https://raw.githubusercontent.com/jafrazier/JSON/master/new.json', true);
