5e80ce171dd2a0b84d315d7696164748c5d1f1c2
jfrazier <johnfrazier1222@gmail.com>
2018-07-23 16:36:26 -0400

    update

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-function getComic() {
+function getComicDc() {
   var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         var myComic = JSON.parse(this.responseText);
-        console.log(myComic.company.dc.batman);
+        var pick = document.getElemetById;
+        pick('book1').innerHTML = myComic.company.dc.batman.name;
+
       }
     };
     xhttp.open('GET', 'https://raw.githubusercontent.com/jafrazier/JSON/master/new.json', true);
     xhttp.send();
 }
+getElemetById
