58897173876ddf138181260f40f0b4432255177b
jfrazier <johnfrazier1222@gmail.com>
2018-07-23 15:42:19 -0400

    chaged JSON

---
+++ main.js
@@ -0,0 +1,11 @@
+function getComic() {
+  var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var myComic = JSON.parse(this.responseText);
+        console.log(myComic.company);
+      }
+    };
+    xhttp.open('GET', 'https://raw.githubusercontent.com/jafrazier/JSON/master/new.json', true);
+    xhttp.send();
+}
